5e2efd1e3730efc74334832f3fd7fcf9ba07c965
Utkarsh Manikrao Nagrale <utkarsh.nagrale@gmail.com>
2023-09-30 10:41:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './component/Home';
+import Header from './component/Header';
+import Professional from './component/Professional';
+import Academics from './component/Academics';
+import Certification from './component/Certification';
+import Project from './component/Project'
+import Newcontact from './component/NewContact';
+import "bootstrap/dist/css/bootstrap.css";
+import About from './component/About';
+
+function App() {
+  return (
+    <div>
+      
+      <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/academics" element={<Academics/>}/>
+        <Route path="/professional" element={<Professional/>}/>
+        <Route path="/project" element={<Project/>} />
+        <Route path="/certification" element={<Certification/>}/>  
+        <Route path="/newcontact" element={<Newcontact/>}></Route>
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/component/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Navbar, Nav} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Header(){
+    let navLinkStyle=({isActive})=>{
+        return{
+            fontWeight: isActive ? 'bold' : 'normal',
+            color: isActive ? 'Red' : 'Black',
+            textDecoration: isActive ? 'none' : 'underline'
+        }
+    }
+
+    return(
+    
+  
+    <Navbar className="navbar navbar-expand-sm navbar-light bg-warning" expand="lg">
+            <Navbar.Brand href="/" className="navbar-brand test-dark m-2"><b>Utkarsh.WebDev</b></Navbar.Brand>
+
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+            <Navbar.Collapse className='collapse navbar-collapse justify-content-end' id="basic-navbar-nav">
+                <Nav activeKey="/">
+                    <Nav.Link  href="/"><b>Home</b></Nav.Link>
+                    <Nav.Link  href="/about"><b>About Me</b></Nav.Link>
+                    <Nav.Link  href="/academics"><b>Academics</b></Nav.Link>
+                    <Nav.Link  href="/professional"><b>Professional</b></Nav.Link>
+                    <Nav.Link  href="/certification"><b>Certification</b></Nav.Link>
+                    <Nav.Link  href="/project"><b>Project</b></Nav.Link>
+                    <Nav.Link  href="/newcontact"><b>Contact</b></Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+    </Navbar>
+    )
+}
+export default Header;
